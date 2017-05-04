6306368d8438a526d4065512abeb4c4171873005
Russell F <Russell.Frame@machiningcloud.com>
2017-05-04 16:05:59 -0700

    Using the Spread Operator

--- app.ts
+++ app.ts
@@ -37,3 +37,27 @@ function LogFavoriteBooks([book1, book2, ...others]: Book[]) {
 // console.log(booktitle);
 // console.log(bookauthor);
 /****************************************/
+
+
+/****************************************/
+//Using the Spread Operator
+/****************************************/
+/****************************************/
+// let schoolBooks: Book[]  = [
+//     { id: 10, title: 'The Greate Gatsby', author: 'F. Scott Fitzgerald', available: true, category: Category.Fiction},
+//     { id: 11, title: 'Crime and Punishment', author: 'Fyod Dostoevsky', available: true, category: Category.Fiction},
+//     { id: 12, title: 'Clear Light of Day', author: 'Anita Desai', available: true, category: Category.Fiction},
+// ];
+//
+// let booksRead: Book[] = util.GetAllBooks();
+// booksRead.push(...schoolBooks);
+// //console.log(booksRead);
+//
+// let poets: string[] = ['Shelley', 'Collins', 'Hughes'];
+// let authors: string[] = ['Tolstoy', 'Fitzgerald', ...poets];
+//
+// let newAuthors: string[] = [...poets, ...authors]
+//
+// console.log(authors);
+
+/****************************************/
